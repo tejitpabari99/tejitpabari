cc959ff941e7a1973b6b348ec051ccbe339c4d47
tejitpabari99 <tvp2107@columbia.edu>
2023-12-22 12:30:44 -0800

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,15 @@
 import * as React from "react"
+import {Flex} from '@chakra-ui/react';
+
 
 import Layout from "../components/common/layout"
 import Seo from "../components/common/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Flex padding={'10vh'} paddingTop={'30vh'} h={'100vh'} display={'block'}>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Flex>
   </Layout>
 )
 
