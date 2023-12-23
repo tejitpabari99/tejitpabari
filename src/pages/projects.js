3a6dedb3358d639dc703c1c2b76d471fcedde69e
tejitpabari99 <tvp2107@columbia.edu>
2023-12-22 19:32:45 -0800

    projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,13 +1,63 @@
 import * as React from "react"
+import Writeup from "../components/common/writeup"
 
 import Layout from "../components/common/layout"
 import Seo from "../components/common/seo"
 
 const ProjectsPage = () => (
   <Layout>
-    
+    <Writeup writeupData={writeupData} heading={"Projects"}/>
   </Layout>
 )
+
+const writeupData = [
+  {
+    "title": "Columbia Virtual Campus",
+    "subtitle": "Co-Founder and CTO",
+    "description": `Conceptualized CVC - a pandemic-born virtual community platform supporting students with resources, events, and initiatives. Led user research, product design and tech stack development using Javascript, Node.js, Firebase, Gatsby and Netlify. Mentored a team of 30 students across 5 projects. Achieved 10,000+ views, with 500+ unique users. Organized community projects such as Alumni Meetups, Freshmen Social, and Black Lives Matters tutoring
+    initiative, raising $1100.`,
+    "links": [
+      {
+        "title": "Website",
+        "link": "https://columbiavirtualcampus.com/"
+      },
+      {
+        "title": "Facebook",
+        "link": "https://www.facebook.com/columbiavirtualcampus/"
+      },
+      {
+        "title": "Instagram",
+        "link": "https://www.instagram.com/columbiavirtualcampus/"
+      },
+    ]
+  },
+  {
+    "title": "Virtual Campus Design Challenge",
+    "subtitle": "Head Organizer",
+    "description": `Conceptualized and executed a three-day hackathon for Columbia students, offered technical mentorship to participants, orchestrated a speaker series featuring distinguished researchers, facilitated mentoring sessions and coordinated the judging process. Over 30 teams participated, with 4 winners in different categories awarded $500. The winning entries were widely used by the Columbia students to foster community interaction, medical support and improve online learning during the COVID crisis.`,
+    "links": [
+      {
+        "title": "Website",
+        "link": "https://columbiavirtualcampus.github.io/"
+      },
+      {
+        "title": "News Coverage",
+        "link": "https://www.engineering.columbia.edu/news/lydia-chilton-eugene-wu-design-challenge-covid"
+      },
+    ]
+  },
+  {
+    "title": "Columbia Space Initiative",
+    "subtitle": "Micro-G Challenge Leader",
+    "description": `Led a team of 6 students to pursue the 2018 Micro-G challenge by NASA.`,
+    "links": [
+      {
+        "title": "News Coverage",
+        "link": "https://www.engineering.columbia.edu/news/csi-spaceposium"
+      }
+    ]
+  }
+]
 
 export const Head = () => <Seo/>
 
